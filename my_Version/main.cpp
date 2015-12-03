f774faa4b669eef5829ed1d5691271c00c45214f
Hongyi Guo <guohongyi1991@gmail.com>
2015-12-03 17:53:30 -0600

    no message

---
+++ my_Version/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  my_Version
+//
+//  Created by Hongyi Guo on 12/3/15.
+//  Copyright © 2015 Hongyi Guo. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
